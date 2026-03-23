1efaaff7584cd17a608f7a143f19123c06271cbc
JE Duch <duchlobo@gmail.com>
2026-03-23 18:04:27 -0500

    fix: explicit vercel adapter injection for serverless build

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,13 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pacto.mx',
   output: 'server',
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   },
